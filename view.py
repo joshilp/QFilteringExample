3cfc19ea9a9a5e62e3c187f2fc2ab05308443769
Joshil Patel <joshil.patel@gmail.com>
2020-04-12 09:50:01 -0700

    cleanup

--- view.py
+++ view.py
@@ -19,12 +19,9 @@ class MyWidget(QWidget):
             self.model.setItem(row, 2, QStandardItem("Joshil_" + text))
             self.model.setItem(row, 3, QStandardItem("Test_" + text))
 
-        # self.filter_proxy_model = QSortFilterProxyModel()
         self.filter_proxy_model = SortFilterProxyModel()
         self.filter_proxy_model.setColumns(len(labels))
-        
         self.filter_proxy_model.setSourceModel(self.model)
-        # self.filter_proxy_model.setFilterKeyColumn(2) # third column
 
         self.lineedit = QLineEdit()
         self.lineedit.textChanged.connect(self.filter_proxy_model.setFilterRegExp)
@@ -39,6 +36,7 @@ class SortFilterProxyModel(QSortFilterProxyModel):
     def __init__(self):
         super(SortFilterProxyModel, self).__init__()
         self.role = Qt.DisplayRole
+        self.columns = 0
 
     def setColumns(self, columns):
         self.columns = columns
@@ -46,7 +44,6 @@ class SortFilterProxyModel(QSortFilterProxyModel):
     def filterAcceptsRow(self, row, parent):
         for index in self._get_indexes(row, parent):
             value = self.sourceModel().data(index, self.role)
-            print(self.filterRegExp())
             if self.filterRegExp().indexIn(str(value)) >= 0:
                 return True
         return False
@@ -55,18 +52,9 @@ class SortFilterProxyModel(QSortFilterProxyModel):
         for col in range(self.columns):
             yield self.sourceModel().index(row, col, parent)
 
-    # def _get_indexes2(self, row, parent):
-    #     index0 = self.sourceModel().index(row, 0, parent)
-    #     index1 = self.sourceModel().index(row, 1, parent)
-    #     index2 = self.sourceModel().index(row, 2, parent)
-    #     index3 = self.sourceModel().index(row, 3, parent)
-    #     return [index0, index1, index2, index3]
-
 if __name__ == "__main__":
     app = QApplication([])
-
     widget = MyWidget()
     widget.resize(800, 600)
     widget.show()
-
     sys.exit(app.exec_())
